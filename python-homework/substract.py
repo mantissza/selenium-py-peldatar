f4a121b388a280294d634bccad4b6d3a828d224d
Dorina Pátyerkó <dorinapatyerko@gmail.com>
2021-06-20 12:16:34 +0200

    016.md/1 hw's solution aka Subtraction

---
+++ python-homework/substract.py
@@ -0,0 +1,11 @@
+# 016.md/1 hw's solution aka Subtraction
+
+print("Subtraction".upper())
+print("Minuend: ")
+x = int(input())
+print("Subtrahend: ")
+y = int(input())
+
+solution = x-y
+print("Difference: %s" % solution)
+print("%s - %s = %s" % (x, y, solution))
