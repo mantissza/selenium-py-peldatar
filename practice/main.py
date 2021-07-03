ff9f1be1344ff5c2b7a9e4be8f365cfaa482b6f2
Dorina Pátyerkó <dorinapatyerko@gmail.com>
2021-07-03 20:36:38 +0200

    practice

---
+++ practice/main.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+driver.get("https://python.org")
+q = driver.find_element_by_name("q")
+q.send_keys("input")
+
+submit = driver.find_element_by_name("submit")
+submit.click()
+
+#driver.close() # Bezárja az ablakot
