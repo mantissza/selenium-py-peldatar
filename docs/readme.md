4556f46b177a44ba0d4002a28c7748c964c89fe5
Dorina Pátyerkó <dorinapatyerko@gmail.com>
2021-07-03 23:47:36 +0200

    practice

--- docs/readme.md
+++ docs/readme.md
@@ -1,1 +1,7 @@
-My first readme doc.
+Docker infos (without WSL 2 !)
+
+1.) docker run -d nginx
+
+2.) docker-compose up
+
+3.) Open in browser || http://localhost:9999/
--- practice/main.py
+++ practice/main.py
@@ -1,11 +1,32 @@
 from selenium import webdriver
 
 driver = webdriver.Chrome()
+''' Multiline comment
 driver.get("https://python.org")
 q = driver.find_element_by_name("q")
 q.send_keys("input")
-
 submit = driver.find_element_by_name("submit")
 submit.click()
+driver.close() # Bezárja az ablakot
+'''
+driver.get("http://localhost:9999/filltablewithsum.html")
 
-#driver.close() # Bezárja az ablakot
+
+def add(p, q, pr):
+    product = driver.find_element_by_id("product")
+    quantity = driver.find_element_by_id("quantity")
+    price = driver.find_element_by_id("price")
+    add_button = driver.find_element_by_id("add")
+
+    product.clear()
+    quantity.clear()
+    price.clear()
+
+    product.send_keys(p)
+    quantity.send_keys(q)
+    price.send_keys(pr)
+    add_button.click()
+
+
+add("Ford", 1, 120000)
+add("Tesla", 3, 12000000)
