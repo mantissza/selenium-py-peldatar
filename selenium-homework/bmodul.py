45350fce78be07eca036e65cf812c53e0b72d404
Dorina Pátyerkó <dorinapatyerko@gmail.com>
2021-07-03 17:51:50 +0200

    003.md hw's solution aka Python moduls

---
+++ selenium-homework/bmodul.py
@@ -0,0 +1,7 @@
+# 003.md hw's solution aka Python moduls
+
+var = 2;
+
+
+def oof(x, y):
+    return x-y;
---
+++ selenium-homework/main.py
@@ -0,0 +1,9 @@
+# 003.md hw's solution aka Python moduls
+
+# import bmodul
+# print(bmodul.oof(2, bmodul.var))
+
+from bmodul import oof, var
+print(oof(2, var))
+
+
