c1ecd854725a40815d8d2c3fb5d21017491de836
Dorina Pátyerkó <dorinapatyerko@gmail.com>
2021-07-04 17:13:56 +0200

    017.md hw's solution aka Selenium webdriver - Navigation - WIP

--- selenium-homework/navigation.py
+++ selenium-homework/navigation.py
@@ -8,28 +8,45 @@ driver.get("http://localhost:9999/general.html")
 list_of_links = driver.find_elements_by_xpath("//a")
 print("Sum of links: " + str(len(list_of_links)))
 
+
+def navigate_to_general():
+    driver.get("http://localhost:9999/general.html")
+
+
+print(list_of_links[9].get_attribute("href"))
+
+
 i = 0
 try:
     for link in list_of_links:
         print(i)
-        driver.execute_script("document.getElementsByTagName('a')[" + str(i) + "].removeAttribute('target');")
+        if link.get_attribute('target'):
+            print("x")
+            driver.execute_script("document.getElementsByTagName('a')[" + str(i) + "].removeAttribute('target');")
         i += 1
         time.sleep(0.2)
-        #link.click()
-        driver.get(link.get_attribute("href"))
-        time.sleep(1)
+        print(link.text)
+        attr = link.get_attribute("href")  # Before navigate
+        link.click()
+        curr = driver.current_url  # After navigate
+        #driver.get(link.get_attribute("href"))
+        time.sleep(0.2)
         # A _blank-ot ugyan eltávolítja a hack, de a külsős url navigálással nem tud mit kezdeni, később visszatérek rá.
         ''' '''
-        if driver.current_url == link.get_attribute("href"):
+        if curr == attr:
             print("OK")
-            print(driver.current_url)
-            print(link.get_attribute("href"))
+            print(curr)
+            print(attr)
+
         else:
             print("Different URLs!")
-            break
+            print(curr)
+            print(attr)
 
         driver.back()
-#except:
-#    print("Error!")
+
+        #driver.get("http://localhost:9999/general.html")
+except:
+    print("Error!")
 finally:
     driver.close()
